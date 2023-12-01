1379c279152f45a2b068d5cc01d7e866eabf76fc
glowingstone124 <hanserofficial@outlook.com>
2023-12-01 23:09:48 +0800

    automatic sql

--- src/main/java/mohist/MohistApplication.java
+++ src/main/java/mohist/MohistApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MohistApplication {
 
     public static void main(String[] args) {
+        Database.init();
         SpringApplication.run(MohistApplication.class, args);
     }
 
